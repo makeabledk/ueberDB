5bb41c340f4f38dbabefaffdd40d7d56494b5909
Nils Henning <nils@makeable.dk>
2024-03-05 13:30:39 +0100

    changed to firebase-admin

--- databases/realtime_db.ts
+++ databases/realtime_db.ts
@@ -1,6 +1,5 @@
 import AbstractDatabase, {Settings} from '../lib/AbstractDatabase';
-import { getDatabase as getRealtimeDatabase, set as firebaseSet, ref as firebaseRef, get as firebaseGet, child,remove } from "firebase/database";
-import { initializeApp, setLogLevel } from "firebase/app";
+import admin from "firebase-admin";
 
 export default class RealtimeDB extends AbstractDatabase {
   public _data: any;
@@ -24,41 +23,20 @@ export default class RealtimeDB extends AbstractDatabase {
   }
 
   async findKeys(key:string, notKey:string) {
-    await this.initFirebase();
-    const tmp = child(firebaseRef(getRealtimeDatabase()), `padsTable`);
-    firebaseGet(tmp).then((snapshot) => {
-        if (snapshot.exists()) {
-            console.log(snapshot.val(),'here');
-        } else {
-            console.log("No data available");
-        }
-    }).catch((error) => {
-        console.error(error);
-    });
-
-
-    try{
       await this.initFirebase();
+      const db = admin.database()
       console.log('findKeys', key, notKey);
-      const docRef = child(firebaseRef(getRealtimeDatabase()), `${this.settings.table ?? 'pads'}`);
-      
-      console.log('docRef', docRef);
-      const values = await firebaseGet(docRef);
-      console.log('values', values.val());
-      const keys = values.val();
+      const ret = await db.ref(`${this.settings.table ?? 'pads'}`).once("value")
+      console.log('findKeys', ret.val());
+      const keys = ret.val();
       const regex = this.createFindRegex(key, notKey);
-      console.log('findKeys', regex, keys, Object.keys(keys).filter((k) => regex.test(k)));
       return Object.keys(keys).filter((k) => regex.test(k));
-    }catch(e){
-      console.log('findKeys', e);
-    }
-    return [];
   }
 
   async get(key:string) {
     await this.initFirebase();
     console.log('get', key);
-    const result = await firebaseGet(child(firebaseRef(getRealtimeDatabase()), `${this.settings.table ?? 'pads'}/${key}`));
+    const result = await admin.database().ref(`${this.settings.table ?? 'pads'}/${key}`).once("value");
     console.log('get', result.val());
     return result.val();
   }
@@ -68,7 +46,7 @@ export default class RealtimeDB extends AbstractDatabase {
   async remove(key:string) {
     await this.initFirebase();
     console.log('remove', key);
-    await remove(firebaseRef(getRealtimeDatabase(), `${this.settings.table ?? 'pads'}/${key}`));
+    await admin.database().ref(`${this.settings.table ?? 'pads'}/${key}`).remove();
     console.log('remove', true);
     return true;
   }
@@ -76,25 +54,15 @@ export default class RealtimeDB extends AbstractDatabase {
   async set(key:string, value:string) {
     await this.initFirebase();
     console.log('set', key, value);
-    await firebaseSet(firebaseRef(getRealtimeDatabase(), `${this.settings.table ?? 'pads'}/${key}`), value);
+    await admin.database().ref(`${this.settings.table ?? 'pads'}/${key}`).set(value);
     console.log('set', true);
   }
 
   async initFirebase() {
     if (!this.firebaseInitialized) {
       console.log('initFirebase', this.settings.clientOptions);
-      setLogLevel('debug');
       this.firebaseInitialized = true;
-      const app = initializeApp({
-        apiKey: this.settings.clientOptions.apiKey,
-        authDomain: this.settings.clientOptions.authDomain,
-        databaseURL: this.settings.clientOptions.databaseURL,
-        projectId: this.settings.clientOptions.projectId,
-        storageBucket: this.settings.clientOptions.storageBucket,
-        messagingSenderId: this.settings.clientOptions.messagingSenderId,
-        appId: this.settings.clientOptions.appId,
-        measurementId: this.settings.clientOptions.measurementId
-      });
+      const app = admin.initializeApp(this.settings.clientOptions);
       console.log('initFirebase done');
     }
   }
